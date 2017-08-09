de5ca46105a93528890d77c12e27b9e3f1aec793
lenovo <lenovo@LAPTOP-PG0T8KA2>
2017-08-09 21:57:07 +0800

    稍微成功一点的版本更新

--- homework/src/main/java/com/red/program/ProgramController.java
+++ homework/src/main/java/com/red/program/ProgramController.java
@@ -1,5 +1,6 @@
 package com.red.program;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProgramController {//节目单管理
+	@Autowired
 	JdbcTemplate jdbcTemplate;
 	
 	/**
@@ -14,7 +16,7 @@ public class ProgramController {//节目单管理
 	 * @param model
 	 * @return
 	 */
-	@RequestMapping("admin")
+	@RequestMapping("program")
 	public String home(Model model) {
 		return "admin";
 	}
